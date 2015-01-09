4de3d11bd9f254cc2b6e3d3ac10c12b8c85c5a9b
Léo Cavaillé <leo@datadoghq.com>
2015-01-09 21:18:54 +0100

    Add sysstat

--- ci/sysstat.rb
+++ ci/sysstat.rb
@@ -4,7 +4,12 @@ namespace :ci do
   namespace :sysstat do
     task :before_install => ['ci:common:before_install']
 
-    task :install => ['ci:common:install']
+    task :install => ['ci:common:install'] do
+      sh %Q{[ -e $HOME/downloads/sysstat-11.0.1.tar.xz || curl -s -L -o $HOME/downloads/sysstat-11.0.1.tar.xz http://perso.orange.fr/sebastien.godard/sysstat-11.0.1.tar.xz}
+      sh %Q{mkdir -p $HOME/sysstat}
+      sh %Q{tar zxf $HOME/downloads/sysstat-11.0.1.tar.xz -C $HOME/sysstat/ --strip-components=1}
+      sh %Q{cd $HOME/sysstat && ./configure && make && make install}
+    end
 
     task :before_script => ['ci:common:before_script']
 
