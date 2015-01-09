d2c8ce32d16c901d106d3957628a61cdecfa9c56
Léo Cavaillé <leo@datadoghq.com>
2015-01-09 22:06:37 +0100

    Install in a different prefix sysstat

--- ci/sysstat.rb
+++ ci/sysstat.rb
@@ -8,7 +8,9 @@ namespace :ci do
       sh %Q{[ -e $HOME/downloads/sysstat-11.0.1.tar.xz ] || curl -s -L -o $HOME/downloads/sysstat-11.0.1.tar.xz http://perso.orange.fr/sebastien.godard/sysstat-11.0.1.tar.xz}
       sh %Q{mkdir -p $HOME/sysstat}
       sh %Q{tar Jxf $HOME/downloads/sysstat-11.0.1.tar.xz -C $HOME/sysstat/ --strip-components=1}
-      sh %Q{cd $HOME/sysstat && ./configure && make && make install}
+      sh %Q{mkdir -p $HOME/embedded}
+      sh %Q{export PATH=$HOME/embedded/bin:$PATH}
+      sh %Q{cd $HOME/sysstat && ./configure --prefix=$HOME/embedded/ && make && make install}
     end
 
     task :before_script => ['ci:common:before_script']
