c41c2e792dd6ca44daa98da66a1ac251611a3382
Léo Cavaillé <leo@datadoghq.com>
2015-01-09 21:54:31 +0100

    gzip is no xz

--- ci/sysstat.rb
+++ ci/sysstat.rb
@@ -7,7 +7,7 @@ namespace :ci do
     task :install => ['ci:common:install'] do
       sh %Q{[ -e $HOME/downloads/sysstat-11.0.1.tar.xz ] || curl -s -L -o $HOME/downloads/sysstat-11.0.1.tar.xz http://perso.orange.fr/sebastien.godard/sysstat-11.0.1.tar.xz}
       sh %Q{mkdir -p $HOME/sysstat}
-      sh %Q{tar zxf $HOME/downloads/sysstat-11.0.1.tar.xz -C $HOME/sysstat/ --strip-components=1}
+      sh %Q{tar Jxf $HOME/downloads/sysstat-11.0.1.tar.xz -C $HOME/sysstat/ --strip-components=1}
       sh %Q{cd $HOME/sysstat && ./configure && make && make install}
     end
 
